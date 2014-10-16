733430b1a0266ab8b4a4fc637f6156c6b71e0de9
rickydo <g3tdo@yahoo.com>
2014-10-16 14:10:46 -0500

    added error message for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 				redirect_to root_path
 			end
 		else
+			flash[:error] = "Incorrect Username or password"
 			redirect_to root_path
 		end
 	end
